86e6924aee2e6a5f1630794c9b9c1edbb8fb3b0a
Harpo Jaeger <harpo.jaeger@gmail.com>
2017-04-27 14:19:44 -0400

    Update syntax to plural nouns

--- utils/api.js
+++ utils/api.js
@@ -1,14 +1,14 @@
 var axios = require('axios')
 
 function completed() {
-  return axios.get('http://localhost:5000/poem/completed')
+  return axios.get('http://localhost:5000/poems/completed')
     .then(function(poems) {
       return poems.data
     })
 }
 
 function random() {
-  return axios.get('http://localhost:5000/poem/random')
+  return axios.get('http://localhost:5000/poems/random')
     .then(function(poem) {
       return poem.data
     })
