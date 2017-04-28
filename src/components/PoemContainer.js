76f768a99fecebda33e78e6b23ffa2ea904764bf
Harpo Jaeger <harpo.jaeger@gmail.com>
2017-04-28 17:40:42 -0400

    Make PoemContainer a stateless functional component

--- src/components/PoemContainer.js
+++ src/components/PoemContainer.js
@@ -3,28 +3,25 @@ var PoemNav = require('./PoemNav')
 var PoemList = require('./PoemList')
 var Loader = require('./Loader')
 var PropTypes = require('prop-types')
-class PoemContainer extends React.Component {
-  render() {
-    // Generate a list of ids for the nav block
-    var everyNth = 25
-    var navIds = []
-    this.props.poems.forEach( (poem) =>
-      ( poem.id % everyNth === 0 || poem.id === 1 )
-      && navIds.unshift(poem.id)
-    )
-    return (
-      <div>
-        <Loader visible={ this.props.poems.length === 0 }>Loading finished poems...</Loader>
-        <div className={this.props.poems.length ? '' : 'hidden'}>
-          <PoemNav ids={navIds} />
-        </div>
-
-        <PoemList poems={this.props.poems} />
+function PoemContainer(props) {
+  // Generate a list of ids for the nav block
+  var everyNth = 25
+  var navIds = []
+  props.poems.forEach( (poem) =>
+    ( poem.id % everyNth === 0 || poem.id === 1 )
+    && navIds.unshift(poem.id)
+  )
+  return (
+    <div>
+      <Loader visible={ props.poems.length === 0 }>Loading finished poems...</Loader>
+      <div className={props.poems.length ? '' : 'hidden'}>
+        <PoemNav ids={navIds} />
       </div>
-    )
-  }
-}
 
+      <PoemList poems={props.poems} />
+    </div>
+  )
+}
 PoemContainer.propTypes = {
   poems: PropTypes.array.isRequired,
 }
