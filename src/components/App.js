e1621c5851433d6229e5b9e674a09f0cc3cc78fe
Harpo Jaeger <harpo.jaeger@gmail.com>
2017-04-28 17:38:46 -0400

    Move poem retriever function to <App />

--- src/components/App.js
+++ src/components/App.js
@@ -3,14 +3,38 @@ var PropTypes = require('prop-types')
 import '../styles/App.css'
 var PoemContainer = require('./PoemContainer')
 var Editor = require('./Editor')
+var api = require('../../utils/api')
 
 
 class App extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      poems: []
+    }
+
+    this.updateCompletedPoems = this.updateCompletedPoems.bind(this)
+  }
+
+  componentDidMount () {
+    this.updateCompletedPoems()
+  }
+
+  updateCompletedPoems() {
+    // Need to move this function into <App /> and figure out how to pass it to <CompletedPoems /> so that <Editor /> has access to it too.
+    api.completed()
+      .then(function(poems) {
+        this.setState({
+          poems: poems
+        })
+      }.bind(this))
+  }
+
   render() {
     return (
       <div>
         <Editor />
-        <PoemContainer />
+        <PoemContainer poems={this.state.poems}/>
       </div>
     )
   }
--- src/components/PoemContainer.js
+++ src/components/PoemContainer.js
@@ -1,50 +1,32 @@
 var React = require('react')
 var PoemNav = require('./PoemNav')
 var PoemList = require('./PoemList')
-var api = require('../../utils/api')
 var Loader = require('./Loader')
-
+var PropTypes = require('prop-types')
 class PoemContainer extends React.Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      poems: []
-    }
-    this.updateCompletedPoems = this.updateCompletedPoems.bind(this)
-  }
-
-  componentDidMount () {
-    this.updateCompletedPoems()
-  }
-
-  updateCompletedPoems() {
-    // Need to move this function into <App /> and figure out how to pass it to <CompletedPoems /> so that <Editor /> has access to it too.
-    api.completed()
-      .then(function(poems) {
-        this.setState({
-          poems: poems
-        })
-      }.bind(this))
-  }
   render() {
     // Generate a list of ids for the nav block
     var everyNth = 25
     var navIds = []
-    this.state.poems.forEach( (poem) =>
+    this.props.poems.forEach( (poem) =>
       ( poem.id % everyNth === 0 || poem.id === 1 )
       && navIds.unshift(poem.id)
     )
     return (
       <div>
-        <Loader visible={ this.state.poems.length === 0 }>Loading finished poems...</Loader>
-        <div className={this.state.poems.length ? '' : 'hidden'}>
+        <Loader visible={ this.props.poems.length === 0 }>Loading finished poems...</Loader>
+        <div className={this.props.poems.length ? '' : 'hidden'}>
           <PoemNav ids={navIds} />
         </div>
 
-        <PoemList poems={this.state.poems} />
+        <PoemList poems={this.props.poems} />
       </div>
     )
   }
+}
+
+PoemContainer.propTypes = {
+  poems: PropTypes.array.isRequired,
 }
 
 module.exports = PoemContainer
