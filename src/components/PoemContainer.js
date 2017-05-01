3f856a00483d75a5a971ae23cbf7a76763c1941a
Harpo Jaeger <harpo.jaeger@gmail.com>
2017-05-01 12:37:28 -0400

    Class-ify PoemContainer

--- src/components/PoemContainer.js
+++ src/components/PoemContainer.js
@@ -3,25 +3,28 @@ var PoemNav = require('./PoemNav')
 var PoemList = require('./PoemList')
 var Loader = require('./Loader')
 var PropTypes = require('prop-types')
-function PoemContainer(props) {
-  // Generate a list of ids for the nav block
-  var everyNth = 25
-  var navIds = []
-  props.poems.forEach( (poem) =>
-    ( poem.id % everyNth === 0 || poem.id === 1 )
-    && navIds.unshift(poem.id)
-  )
-  return (
-    <div>
-      <h3>Completed poems</h3>
-      <Loader visible={ props.poems.length === 0 }>Loading...</Loader>
-      <div className={props.poems.length ? '' : 'hidden'}>
-        <PoemNav ids={navIds} />
-      </div>
 
-      <PoemList poems={props.poems} />
-    </div>
-  )
+class PoemContainer extends React.Component {
+  render() {
+    // Generate a list of ids for the nav block
+    var everyNth = 25
+    var navIds = []
+    this.props.poems.forEach( (poem) =>
+      ( poem.id % everyNth === 0 || poem.id === 1 )
+      && navIds.unshift(poem.id)
+    )
+    return (
+      <div>
+        <h3>Completed poems</h3>
+        <Loader visible={ this.props.poems.length === 0 }>Loading...</Loader>
+        <div className={this.props.poems.length ? '' : 'hidden'}>
+          <PoemNav ids={navIds} />
+        </div>
+
+        <PoemList poems={this.props.poems} />
+      </div>
+    )
+  }
 }
 PoemContainer.propTypes = {
   poems: PropTypes.array.isRequired,
